2d03abf6f81d4bc477ee97dacee51b3670b31e71
Subhajit Ghosh <137408509+Subhajit75@users.noreply.github.com>
2025-08-04 19:17:20 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
-# Install Chrome and ChromeDriver in Render for Selenium
+# Install Chrome & Chromedriver for Render
 
-echo "---- Installing Google Chrome ----"
 apt-get update
-apt-get install -y wget unzip curl
+apt-get install -y wget unzip curl gnupg
 
-# Install Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+# Install Google Chrome
+curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
+apt install -y ./chrome.deb
 
-# Install ChromeDriver (latest)
-CHROME_VERSION=$(google-chrome --version | sed 's/Google Chrome //g' | cut -d '.' -f 1)
-DRIVER_VERSION=$(curl -sS "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-wget -N "https://chromedriver.storage.googleapis.com/${DRIVER_VERSION}/chromedriver_linux64.zip" -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-mv -f ~/chromedriver /usr/local/bin/chromedriver
-chmod 0755 /usr/local/bin/chromedriver
-
-echo "---- Chrome and ChromeDriver installed ----"
+# Confirm versions
+google-chrome --version
